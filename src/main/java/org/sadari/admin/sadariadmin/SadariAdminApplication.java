48e312c0b23fea53a87139933d7ae7c8aba89775
Vella <112395421+vellahw@users.noreply.github.com>
2026-07-07 23:37:39 +0900

    first commit

---
+++ src/main/java/org/sadari/admin/sadariadmin/SadariAdminApplication.java
@@ -0,0 +1,13 @@
+package org.sadari.admin.sadariadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadariAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SadariAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sadari/admin/sadariadmin/SadariAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sadari.admin.sadariadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SadariAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
